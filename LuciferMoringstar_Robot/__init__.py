2466e5cf7bb92a1e9d8f290076d2004e0affa603
jokermon <85845434+jokermon@users.noreply.github.com>
2021-09-30 09:21:05 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -27,17 +27,17 @@ Bot Owner Only
 
 [➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
 
-@Mo_Tech_YT
+@cinehut 
 """
 
 ABOUT = """
-➥ Name :  Auto Filter With IMDB
+➥ Name :  Ruby
     
-➥ Creator : [Muhammed RK](t.me/Mrk_YT)
+➥ Creator : [Joker](t.me/Angelamego)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+➥ Source Code : [പേടിക്കേണ്ട എല്ലാവർക്കും ഉണ്ട് 😅](https://t.me/onnpoderka/2)
 """
